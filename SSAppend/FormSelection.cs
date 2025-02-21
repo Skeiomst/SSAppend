98d5b22db367722291ac4e4b0c20d7ec7b213292
Skeiomst <samuelpalfaro@gmail.com>
2025-02-20 22:22:31 -0600

    First Versión

---
+++ SSAppend/FormSelection.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace SSAppend
+{
+    public class FormSelection : Form
+    {
+        #region Properties
+        
+        private Point start;
+        private Point end;
+        private bool selecting = false;
+        private Cursor originalCursor;
+
+        #endregion Properties
+
+        public FormSelection()
+        {
+            this.BackColor = Color.Black;
+            this.Opacity = 0.5;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.DoubleBuffered = true;
+            this.MouseDown += new MouseEventHandler(FormSelection_MouseDown);
+            this.MouseMove += new MouseEventHandler(FormSelection_MouseMove);
+            this.MouseUp += new MouseEventHandler(FormSelection_MouseUp);
+
+            originalCursor = Cursor.Current;
+            this.Cursor = Cursors.Cross;
+        }
+
+        public Rectangle SelectArea()
+        {
+            this.ShowDialog();
+            return new Rectangle(
+                Math.Min(start.X, end.X),
+                Math.Min(start.Y, end.Y),
+                Math.Abs(end.X - start.X),
+                Math.Abs(end.Y - start.Y)
+                );
+        }
+
+        private void FormSelection_MouseDown(object sender, MouseEventArgs e)
+        {
+            selecting = true;
+            start = e.Location;
+        }
+
+        private void FormSelection_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selecting)
+            {
+                end = e.Location;
+                this.Invalidate();
+            }
+        }
+
+        private void FormSelection_MouseUp(object sender, MouseEventArgs e)
+        {
+            selecting = false;
+            end = e.Location;
+
+            this.Cursor = originalCursor;
+
+            this.Close();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using (Brush semiTransparent = new SolidBrush(Color.FromArgb(150, 0, 0, 0)))
+            {
+                e.Graphics.FillRectangle(semiTransparent, this.ClientRectangle);
+            }
+            if (selecting)
+            {
+                Rectangle select = new Rectangle(
+                    Math.Min(start.X, end.X),
+                    Math.Min(start.Y, end.Y),
+                    Math.Abs(end.X - start.X),
+                    Math.Abs(end.Y - start.Y)
+                );
+
+
+                using (GraphicsPath path = new GraphicsPath())
+                {
+                    path.AddRectangle(this.ClientRectangle);
+                    path.AddRectangle(select);
+                    e.Graphics.FillPath(Brushes.Transparent, path);
+                }
+
+                using (Pen pen = new Pen(Color.Red, 2))
+                {
+                    e.Graphics.DrawRectangle(pen, select);
+                }
+            }      
+        }
+    }
+}
--- SSAppend/MainWindow.xaml.cs
+++ SSAppend/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
 
 namespace SSAppend
 {
@@ -20,9 +12,101 @@ namespace SSAppend
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Properties
+        private List<Bitmap> _screenshots = new List<Bitmap>();
+        private Bitmap finalImage;
+        #endregion Properties
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            CaptureScreen();
+        }
+
+        private void CaptureScreen()
+        {
+            this.Hide();
+            System.Threading.Thread.Sleep(300);
+
+            System.Drawing.Rectangle area = SelectArea();
+            if (area.Width > 0 && area.Height > 0)
+            {
+                Bitmap screenshot = CaptureWindow(area);
+                _screenshots.Add(screenshot);
+                finalImage = AppendCaptures();
+                Clipboard.SetImage(BitmapCovert(finalImage));
+                imgPreview.Source = BitmapCovert(finalImage);
+            }
+            this.Show();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            _screenshots.Clear();
+            finalImage = null;
+            imgPreview.Source = null;
+        }
+
+        private System.Drawing.Rectangle SelectArea()
+        {
+            using (var selection = new FormSelection())
+            {
+                return selection.SelectArea();
+            }
+        }
+
+        private Bitmap CaptureWindow(System.Drawing.Rectangle area)
+        {
+            Bitmap screenshot = new Bitmap(area.Width, area.Height);
+            using (Graphics gfx = Graphics.FromImage(screenshot))
+            {
+                gfx.CopyFromScreen(area.Location, System.Drawing.Point.Empty, area.Size);
+            }
+            return screenshot;
+        }
+
+        private Bitmap AppendCaptures()
+        {
+            if (_screenshots.Count == 0) return null;
+
+            int maxWidth = 0;
+            int totalHeight = 0;
+
+            foreach (var img in _screenshots)
+            {
+                maxWidth = Math.Max(maxWidth, img.Width);
+                totalHeight += img.Height;
+            }
+
+            Bitmap result = new Bitmap(maxWidth, totalHeight);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                int yOffset = 0;
+                foreach (var img in _screenshots)
+                {
+                    g.DrawImage(img, 0, yOffset);
+                    yOffset += img.Height;
+                }
+            }
+            return result;
+        }
+
+        private BitmapImage BitmapCovert(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                ms.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
         }
     }
 }
